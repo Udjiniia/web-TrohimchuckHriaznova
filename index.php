6fbdc3e9d0f512ffe80e88a12f4108988a8f6a8f
Udjiniia <jeniagriaznova@gmail.com>
2020-12-26 00:20:11 +0200

    Лаба 6

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <link rel="shortcut icon" type="image/x-icon" href="images/book.png"/>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Alegreya&family=Caveat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#datepicker" ).datepicker();
+            $( "#dialog" ).dialog();
+        } );
+    </script>
 </head>
 <style>
     ul#ul {
@@ -16,14 +26,26 @@
     ul#ul1 {
         list-style-image: url("images/bestseller.png");
     }
+    .php_answer{
+        background-color: #eeeeee;
+        text-align: right;
+    }
+    .tweet{
+        background-color: #eeeeee;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+    div p{
+        padding-left: 10px;
+        font-size: 15px;
+    }
 </style>
 <body>
-
-<script>
-    var date = new Date();
-    alert(date);
-</script>
-
+<div id="dialog" title="Books Online">
+    <p> Ласкаво просимо до найкращої віртуальної бібліотеки Books Online! Бажаємо захопливої подорожі до світу книжок.
+        Вікно можна закрити, натиснувши на &apos;x&apos;.</p>
+</div>
 <div class="container">
     <header>
         <div class="header">
@@ -38,12 +60,19 @@
                 </ul>
             </nav>
             <form class="search">
-                <input type="search" placeholder="Пошук">
-                <input type="submit" onclick="window.alert('Пошук недоступний')" value="Знайти">
+                <input type="search" name="search" placeholder="Пошук">
+                <input type="submit" value="Знайти">
             </form>
         </div>
     </header>
-
+    <div class="php_answer">
+        <?php
+        if (( $search = $_GET['search'])) {
+            $search = $_GET['search'];
+            echo('За вашим запитом "'.$search.'" нічого не занйдено');
+        }
+        ?>
+    </div>
     <div class="main">
         <div class="best-lib">
             <ul id="ul">
@@ -57,6 +86,7 @@
                 <li>Психологія</li>
                 <li>Романи</li>
             </ul>
+            <p>Дата: <input type="text" id="datepicker"></p>
         </div>
         <div class="best-today">
             <ul id="ul1">
@@ -68,6 +98,11 @@
             </ul>
         </div>
     </div>
+    <div class="tweet">
+        <blockquote class="twitter-tweet"><p lang="und" dir="ltr"><a href="https://t.co/dtZt2Tju9T">pic.twitter.com/dtZt2Tju9T</a></p>&mdash; ᴛᴡɪʟɪɢʜᴛ ʀᴇɴᴀɪssᴀɴᴄᴇ (@twilightreborn) <a href="https://twitter.com/twilightreborn/status/1317080352494673925?ref_src=twsrc%5Etfw">October 16, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <blockquote class="twitter-tweet"><p lang="ru" dir="ltr">у кого какая любимая лгбт драма?</p>&mdash; Юрий Каплан (@yurakaplan) <a href="https://twitter.com/yurakaplan/status/1276430754843693056?ref_src=twsrc%5Etfw">June 26, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
     <div class="bottom">
         <h5>Повідомити про нове надходження книг на електронну пошту:  </h5>
         <form action="index.php"  method="POST">
@@ -82,6 +117,7 @@
         }
         ?>
         <br>
+
         <p class="bottom-text">“The library is inhabited by spirits that come out of the pages at night.”
             – <strong>Isabel Allende</strong></p>
         <br>
@@ -91,6 +127,7 @@
             <li><strong>Геній</strong> Мо Янь</li>
             <li><strong>Двобій</strong> Кліффорд Саймак</li>
         </ul>
+
     </div>
 
     <footer>
@@ -102,9 +139,8 @@
             $offset = 2;
             $time += 2 * 3600;
             echo date("d-m-Y H:i:s", $time);
-            ?></div>
+            ?>
         </div>
     </footer>
-</div>
 </body>
 </html>
